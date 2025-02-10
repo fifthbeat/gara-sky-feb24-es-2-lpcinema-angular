fc995389603a2398dfb1cc9e414e64175e16f082
tommaso frosini <t.frosini@dstech.it>
2025-02-10 16:22:45 +0100

    small fix for testing setup

--- src/app/components/carousel/carousel.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { CarouselComponent } from './carousel.component'
-
-describe('CarouselComponent', () => {
-  it('should mount', () => {
-    cy.mount(CarouselComponent)
-  })
-})
--- src/app/components/hero-cards/hero-card/hero-card.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { HeroCardComponent } from './hero-card.component'
-
-describe('HeroCardComponent', () => {
-  it('should mount', () => {
-    cy.mount(HeroCardComponent)
-  })
-})
