2929796b84c9290495ba2d62cf7e99536e0c17ac
Denis Lo Presti <d.lopresti@dstech.it>
2025-02-11 18:15:59 +0100

    prima implementazione hero-cta

--- src/app/components/price/price.component.ts
+++ src/app/components/price/price.component.ts
@@ -1,13 +1,23 @@
 import {Component, Input} from '@angular/core';
+import {SplitPricePipe} from '../../pipe/split-price.pipe';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-price',
-  imports: [],
+  imports: [
+    CommonModule,
+    SplitPricePipe
+  ],
   standalone: true,
   templateUrl: './price.component.html',
   styleUrl: './price.component.scss'
 })
 export class PriceComponent {
   @Input() price!: number;
+  @Input('text-price') textPrice?: string;
+
+  constructor() {
+  }
+
 
 }
