9fff4acef5f1a2fc7c7df01822d06059777967be
tommaso frosini <t.frosini@dstech.it>
2025-02-11 14:28:32 +0100

    updated app-component for mocks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,18 @@ import { ContentStackService } from './services/content-stack.service';
 import { HeaderComponent } from "./components/header/header.component";
 import { StackEntry } from './models/stack-entry';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeroCtaComponent } from "./components/hero-cta/hero-cta.component";
+import { Banner3ImagesComponent } from "./components/banner-3-images/banner-3-images.component";
+import { TitleComponent } from "./components/title/title.component";
+import { CarouselComponent } from "./components/carousel/carousel.component";
+import { OfferBoxComponent } from "./components/offer-box/offer-box.component";
+import { HeroCardsComponent } from "./components/hero-cards/hero-cards.component";
+import { AccordionComponent } from "./components/accordion/accordion.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, FooterComponent, HeroCtaComponent, Banner3ImagesComponent, TitleComponent, CarouselComponent, OfferBoxComponent, HeroCardsComponent, AccordionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
