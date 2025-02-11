f24788c162c1a31389f074c5df86bad3662b602f
Denis Lo Presti <d.lopresti@dstech.it>
2025-02-11 16:20:30 +0100

    start hero cta code

--- src/app/components/hero-cta/hero-cta.component.ts
+++ src/app/components/hero-cta/hero-cta.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {PriceComponent} from '../price/price.component';
 
 @Component({
   selector: 'app-hero-cta',
-  imports: [],
+  imports: [
+    PriceComponent
+  ],
   standalone: true,
   templateUrl: './hero-cta.component.html',
   styleUrl: './hero-cta.component.scss'
