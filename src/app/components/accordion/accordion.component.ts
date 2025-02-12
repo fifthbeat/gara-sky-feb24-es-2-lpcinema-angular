cf736f9a6ea8c7919db91f7ea759be711222136c
tommaso frosini <t.frosini@dstech.it>
2025-02-12 14:30:21 +0100

    accordion & upgrade color selector

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { AccordionGroup } from '../../models/stack-entry';
 import { CommonModule } from '@angular/common';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-accordion',
-  imports: [CommonModule],
+  imports: [CommonModule, NgbAccordionModule],
   standalone: true,
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.scss'
