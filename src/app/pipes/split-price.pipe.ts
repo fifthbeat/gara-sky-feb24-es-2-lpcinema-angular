43d45f92781363e70d1d42356b5ef782190e81bc
tommaso frosini <t.frosini@dstech.it>
2025-02-13 12:27:41 +0100

    varies small fixes

--- src/app/pipes/split-price.pipe.ts
+++ src/app/pipes/split-price.pipe.ts
@@ -1,7 +1,8 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
-  name: 'splitPrice'
+  name: 'splitPrice',
+  standalone: true
 })
 export class SplitPricePipe implements PipeTransform {
 
